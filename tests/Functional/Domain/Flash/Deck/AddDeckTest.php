cf23d12f7f5934b75b817ff2a3e4de8055f2c67b
ignashov-roman@mail.ru <WyybsK6wTUGmxmu>
2021-04-04 14:57:32 +0300

    TEST тест добавления коллекции

--- tests/Functional/Domain/Flash/Deck/AddDeckTest.php
+++ tests/Functional/Domain/Flash/Deck/AddDeckTest.php
@@ -2,80 +2,34 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\DeckController;
+namespace App\Tests\Functional\Domain\Flash\Deck;
 
-use App\DataFixtures\CardFixture;
-use App\DataFixtures\DeckFixture;
-use App\DataFixtures\UserFixtures;
-
+use App\DataFixtures\Flash\DeckFixtures;
 use App\Tests\AbstractTest;
 use Symfony\Component\HttpFoundation\Response;
 
 class AddDeckTest extends AbstractTest
 {
-    private $method = 'POST';
-    private $uri = '/decks';
+    protected $method = 'POST';
+    protected $uri = '/flash/deck/add/';
 
     public function getFixtures() : array
     {
-        return [
-            UserFixtures::class,
-            DeckFixture::class,
-            CardFixture::class
-        ];
-    }
-
-    public function setUp() : void
-    {
-        parent::setUp();
+        return [DeckFixtures::class];
     }
 
     public function testPostDeckValid() : void
     {
-        $client = $this->makeRequest($this->method, $this->uri, [
+        $this->makeRequestWithAuth([
             'name' => 'deckName',
             'description' => 'description',
-            'limit_repeat' => 20,
-            'limit_learning' => 20,
-            'difficulty_index' => 1,
-            'base_index' => 1,
-            'modifier_index' => 1
         ]);
 
-        /** @var Response $response */
-        $response = $client->getResponse();
-        $content = json_decode($response->getContent(), true);
-
-        $this->assertResponseCode(Response::HTTP_CREATED, $response);
-        $this->assertArrayHasKey('id', $content);
-        $this->assertArrayHasKey('name', $content);
-        $this->assertArrayHasKey('description', $content);
-        $this->assertArrayHasKey('limit_repeat', $content);
-        $this->assertArrayHasKey('limit_learning', $content);
-        $this->assertArrayHasKey('difficulty_index', $content);
-        $this->assertArrayHasKey('base_index', $content);
-        $this->assertArrayHasKey('modifier_index', $content);
-        $this->assertArrayHasKey('created_at', $content);
-        $this->assertArrayHasKey('updated_at', $content);
-    }
-
-    public function testPostDeckInvalidKey() : void
-    {
-        $client = $this->makeRequest($this->method, $this->uri, [
-            'invalid' => 'invalid',
-        ]);
-
-        /** @var Response $response */
-        $response = $client->getResponse();
-        $content = json_decode($response->getContent(), true);
-        $this->assertResponseCode(Response::HTTP_UNPROCESSABLE_ENTITY, $response);
-        $this->assertArrayHasKey('errors', $content);
-        $content = $content['errors'];
-        $this->assertArrayHasKey('name', $content);
-        $this->assertArrayHasKey('limit_repeat', $content);
-        $this->assertArrayHasKey('limit_learning', $content);
-        $this->assertArrayHasKey('difficulty_index', $content);
-        $this->assertArrayHasKey('base_index', $content);
-        $this->assertArrayHasKey('modifier_index', $content);
+        $this->assertResponseCode(Response::HTTP_CREATED, $this->response);
+        $this->assertArrayHasKey('id', $this->content);
+        $this->assertArrayHasKey('name', $this->content);
+        $this->assertArrayHasKey('description', $this->content);
+        $this->assertArrayHasKey('createdAt', $this->content);
+        $this->assertArrayHasKey('updatedAt', $this->content);
     }
 }
