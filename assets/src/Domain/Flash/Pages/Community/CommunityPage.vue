a92c6e859934287bc9c53e94ca0c140fb1b34a24
ignashov-roman@mail.ru <WyybsK6wTUGmxmu>
2021-08-22 02:25:23 +0300

    FEATURE small fixes for community pages

--- assets/src/Domain/Flash/Pages/Community/CommunityPage.vue
+++ assets/src/Domain/Flash/Pages/Community/CommunityPage.vue
@@ -1,30 +1,32 @@
 <template>
   <v-layout align-center justify-center>
-    <v-card v-if="isMainPage" class="mx-auto" max-width="900">
-      <v-container fluid>
-        <v-row dense>
-          <v-col v-for="page in pages" :key="page.title" :cols="page.flex">
-            <v-card>
-              <v-img
-                  :src="page.src"
-                  class="white--text align-end"
-                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                  height="200px"
-              >
-                <v-card-title v-text="page.title"></v-card-title>
-              </v-img>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn :to="{name: 'CommunityCollection'}">
-                  Перейти<v-icon>{{ 'mdi-chevron-right' }}</v-icon>
-                </v-btn>
-                <!--                <v-btn icon><v-icon>mdi-share-variant</v-icon></v-btn>-->
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
+    <v-row v-if="isMainPage" justify="center">
+      <v-col cols="7">
+      <v-card class="mx-auto" max-width="900">
+        <v-container fluid>
+          <v-row dense>
+            <v-col v-for="page in pages" :key="page.title" :cols="page.flex">
+              <v-card>
+                <v-img
+                    :src="page.src"
+                    class="white--text align-end"
+                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                    height="200px"
+                >
+                  <div style="display: flex; justify-content: space-between; padding: 20px">
+                    <v-card-title v-text="page.title"></v-card-title>
+                    <v-btn :to="{name: 'CommunityCollection'}" style="position: relative; margin-top: 10px">
+                      Перейти<v-icon>{{ 'mdi-chevron-right' }}</v-icon>
+                    </v-btn>
+                  </div>
+                </v-img>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+      </v-col>
+    </v-row>
     <router-view></router-view>
   </v-layout>
 </template>
@@ -35,7 +37,7 @@ import { Component, Vue } from 'vue-property-decorator';
 export default class CommunityPage extends Vue{
     pages = [
       {
-        title: 'Коллекции',
+        title: 'Коллекции сообщества',
         src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
         flex: 12
       }
