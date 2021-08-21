8be16c20ee78c6733bb9ff3cc463f4ddd65a6e96
ignashov-roman@mail.ru <WyybsK6wTUGmxmu>
2021-08-22 02:09:09 +0300

    FEATURE navbar second level

--- assets/src/Domain/App/Layouts/Components/Navbar.vue
+++ assets/src/Domain/App/Layouts/Components/Navbar.vue
@@ -8,7 +8,30 @@
         :to="{ name: link.name}"
         class="navbar-link d-flex justify-center align-center pr-3 pl-3"
         active-class="navbar-link--active"
-      ><v-icon class="mr-3">{{ link.meta.icon }}</v-icon>{{ link.meta.label }}
+        exact
+      >
+        <span v-if="!(link.children && link.children.length)">
+          <v-icon class="mr-3">{{ link.meta.icon }}</v-icon>{{ link.meta.label }}
+        </span>
+
+        <v-menu v-else offset-y open-on-hover>
+          <template v-slot:activator="{ on, attrs }">
+            <span v-bind="attrs" v-on="on">
+              <v-icon class="mr-3">{{ link.meta.icon }}</v-icon>{{ link.meta.label }}
+            </span>
+          </template>
+          <v-list style="margin-top: 20px" color="primary">
+            <v-list-item v-for="(second, index) in link.children" :key="index">
+              <router-link
+                  class="navbar-link d-flex justify-center align-center pr-3 pl-3"
+                  active-class="navbar-link--active"
+                  :to="{ name: second.name}"
+              >
+                <v-icon class="mr-3">{{ second.meta.icon }}</v-icon>{{ second.meta.label }}
+              </router-link>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </router-link>
     </v-list>
   </v-toolbar-items>
--- assets/src/Domain/Flash/Pages/Community/CommunityPage.vue
+++ assets/src/Domain/Flash/Pages/Community/CommunityPage.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout align-center justify-center>
-    <v-card class="mx-auto" max-width="900">
+    <v-card v-if="isMainPage" class="mx-auto" max-width="900">
       <v-container fluid>
         <v-row dense>
           <v-col v-for="page in pages" :key="page.title" :cols="page.flex">
@@ -18,13 +18,14 @@
                 <v-btn :to="{name: 'CommunityCollection'}">
                   Перейти<v-icon>{{ 'mdi-chevron-right' }}</v-icon>
                 </v-btn>
-<!--                <v-btn icon><v-icon>mdi-share-variant</v-icon></v-btn>-->
+                <!--                <v-btn icon><v-icon>mdi-share-variant</v-icon></v-btn>-->
               </v-card-actions>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
     </v-card>
+    <router-view></router-view>
   </v-layout>
 </template>
 <script lang="ts">
@@ -39,5 +40,9 @@ export default class CommunityPage extends Vue{
         flex: 12
       }
     ];
+
+    get isMainPage() {
+      return this.$route.matched.length === 1
+    }
 }
 </script>
